a9901498faf10fe305dfcbd5c63ea1259a371c96
Sergio Gómez Del Real <sergiogomezdelreal@Sergios-Mac.local>
2017-05-06 16:27:29 -0500

    working vm exit

--- monitor.c
+++ monitor.c
@@ -10,22 +10,32 @@
 #include <Hypervisor/hv_vmx.h>
 
 #define SEGM_SIZE 0xFFFF // size of real-mode segment
-#define CTRL_UNRSTR (1<<7) // To execute real-mode code
+
+/* primary processor-based ctrl */
 #define CTRL_HALT_INSTR (1<<7) // VM exit when guest executes halt instruction
-#define CTRL_UNCON_IO (1<<24) // VM exit when guest executes IO instruction
+#define CTRL_CR8_LOAD (1<<19)
+#define CTRL_CR8_STORE (1<<20)
 #define CTRL_SECOND (1<<31) // enable secondary processor controls
 #define PRI_DEF1 (0x401E172) // default1's for primary
-#define PIN_DEF1 (0x16)
-#define PIN_BITMAP (PIN_DEF1 | 0x1)
-#define PROC1_BITMAP (PRI_DEF1 | CTRL_HALT_INSTR | CTRL_UNCON_IO | CTRL_SECOND) // primary processor-based ctrl
+#define PROC1_BITMAP (PRI_DEF1 | CTRL_HALT_INSTR | CTRL_UNCON_IO | CTRL_SECOND | CTRL_CR8_LOAD | CTRL_CR8_STORE) 
+
+/* secondary processor-based ctrl */
+#define CTRL_UNRSTR (1<<7) // To execute real-mode code
 #define PROC2_BITMAP CTRL_UNRSTR
 
+/* pin-based ctrl */
+#define CTRL_UNCON_IO (1<<24) // VM exit when guest executes IO instruction
+#define PIN_DEF1 (0x16)
+#define PIN_BITMAP (PIN_DEF1 | 0x1)
+
+/* map guest physical address to host virtual address */
 #define VM_MEM_MAP(uva, gpa, size, flags) \
  if ( (ret = hv_vm_map(uva, gpa, size, flags) != HV_SUCCESS)) { \
    print_err(ret); \
    exit(0); \
  }
 
+/* execute vm represented by vcpu */
 #define HV_EXEC(vcpu) \
  if ( (ret = hv_vcpu_run(*vcpu)) != HV_SUCCESS) { \
   print_err(ret); \
@@ -93,7 +103,7 @@ static void vmcs_init_ctrl(hv_vcpuid_t *vcpu)
  /* set default1 bits and some capabilities */
 
  read_caps(HV_VMX_CAP_PINBASED, &cap);
- write_vmcs(vcpu, VMCS_CTRL_PIN_BASED, PIN_BITMAP | ((cap & 0xffffffff) & (cap >> 32))); // VM-exit on external interrupts
+ write_vmcs(vcpu, VMCS_CTRL_PIN_BASED, PIN_BITMAP | ((cap & 0xffffffff) & (cap >> 32)));
 
  read_caps(HV_VMX_CAP_PROCBASED, &cap);
  write_vmcs(vcpu, VMCS_CTRL_CPU_BASED, (PROC1_BITMAP | (cap & 0xffffffff)) & (cap >> 32));
@@ -108,7 +118,6 @@ static void vmcs_init_ctrl(hv_vcpuid_t *vcpu)
  write_vmcs(vcpu, VMCS_CTRL_VMEXIT_CONTROLS, (0x36dff | (cap & 0xffffffff)) & (cap >> 32));
 }
 
-/* incomplete */
 static void vmcs_init_guest(hv_vcpuid_t *vcpu)
 {
  write_vmcs(vcpu, VMCS_GUEST_RIP, 0x100); // load program segment at segment:offset -> 0x0:0x100
@@ -150,15 +159,20 @@ static void vmcs_init_guest(hv_vcpuid_t *vcpu)
  write_vmcs(vcpu, VMCS_GUEST_LDTR_LIMIT, SEGM_SIZE);
  write_vmcs(vcpu, VMCS_GUEST_LDTR_AR, 0x82);
  write_vmcs(vcpu, VMCS_GUEST_GDTR_BASE, 0);
+ write_vmcs(vcpu, VMCS_GUEST_GDTR_LIMIT, SEGM_SIZE);
  write_vmcs(vcpu, VMCS_GUEST_IDTR_BASE, 0);
+ write_vmcs(vcpu, VMCS_GUEST_IDTR_LIMIT, SEGM_SIZE);
 
- write_vmcs(vcpu, VMCS_GUEST_CR0, 0x60000010); // in particular, PE and PG disabled; execute in real-mode
+ write_vmcs(vcpu, VMCS_GUEST_CR0, 0x20); // in particular, PE and PG disabled; execute in real-mode
  write_vmcs(vcpu, VMCS_GUEST_CR3, 0x0);
  write_vmcs(vcpu, VMCS_GUEST_DR7, 0x0);
- write_vmcs(vcpu, VMCS_GUEST_SYSENTER_EIP, 0x6826);
- write_vmcs(vcpu, VMCS_GUEST_SYSENTER_ESP, 0x6824);
  write_vmcs(vcpu, VMCS_GUEST_CR4, 1L<<13);
 
+ write_vmcs(vcpu, VMCS_CTRL_EXC_BITMAP, 0xffffffff);
+ write_vmcs(vcpu, VMCS_CTRL_CR0_MASK, 0x60000000);
+ write_vmcs(vcpu, VMCS_CTRL_CR0_SHADOW, 0x0);
+ write_vmcs(vcpu, VMCS_CTRL_CR4_MASK, 0x0);
+ write_vmcs(vcpu, VMCS_CTRL_CR4_SHADOW, 0x0);
 }
 
 int main(int argc, char *argv[])
@@ -205,18 +219,20 @@ int main(int argc, char *argv[])
 
  /* Main loop: execute guest until VM exit */
  uint64_t exit_reas, err;
- while (1) {
+ int stop = 0;
+ while (!stop) {
   HV_EXEC(&vcpu);
   read_vmcs(&vcpu, VMCS_RO_EXIT_REASON, &exit_reas);
-  //read_vmcs(&vcpu, VMCS_RO_EXIT_QUALIFIC, &exit_reas); // for debugging
-  //read_vmcs(&vcpu, VMCS_RO_GUEST_LIN_ADDR, &exit_reas); // for debugging
   switch (exit_reas) {
    case VMX_REASON_HLT:
-    ;
-   case VMX_REASON_IO:
-    ;
+    printf("HLT\n");
+    stop = 1;
+    break;
+   case VMX_REASON_IRQ:
+    printf("INTERRUPT\n");
+    break;
    case VMX_REASON_EPT_VIOLATION:
-    hv_vm_protect(0x100, 0x100, HV_MEMORY_EXEC);
+    ;
    default:
     ;
   }
